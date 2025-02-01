f04ca596e4a125bdd2f0ef83f3cfd7df122dc980
namdaeun <nde40345@gmail.com>
2025-02-01 17:15:13 +0900

    init: svgr 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/",
+  plugins: [
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+    react({
+      jsxImportSource: "@emotion/react",
+    })],
 })
