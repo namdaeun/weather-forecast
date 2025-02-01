0fc66246c611761bbed2156ab6f1477908645657
namdaeun <nde40345@gmail.com>
2025-02-01 16:19:34 +0900

    init: biome 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const element = document.getElementById('root');
+
+if (!element) {
+	throw new Error('Root element not found.');
+}
+
+const root = createRoot(element);
+
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 );
