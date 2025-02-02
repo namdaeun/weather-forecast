16c304637094c1ddfe5d09b396dd21a52605b939
namdaeun <nde40345@gmail.com>
2025-02-03 05:23:43 +0900

    chore: tsconfig 옵션 및 indent 수정

--- src/components/Tab/index.tsx
+++ src/components/Tab/index.tsx
@@ -1,10 +1,10 @@
-import TabContent from './TabContent/TabContent';
-import TabGroup from './TabGroup/TabGroup';
-import TabList from './TabList/TabList';
-import TabMenu from './TabMenu/TabMenu';
+import TabMenu from './TabButton';
+import TabContent from './TabContent';
+import TabGroup from './TabGroup';
+import TabList from './TabList';
 
 export const Tab = Object.assign(TabGroup, {
-	List: TabList,
-	Menu: TabMenu,
-	Content: TabContent,
+ List: TabList,
+ Menu: TabMenu,
+ Content: TabContent,
 });
