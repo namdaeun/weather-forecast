1dda2fba7b9716d3f265ac1d578e936924820795
namdaeun <nde40345@gmail.com>
2025-02-04 23:24:53 +0900

    fix: navbar 삭제

--- src/assets/svg/icon/index.ts
+++ src/assets/svg/icon/index.ts
@@ -1,3 +1,4 @@
 export { default as IcMap } from "./IcMap";
 export { default as IcSearch } from "./IcSearch";
+export { default as IcSun } from "./IcSun";
 export { default as IcWeather } from "./IcWeather";
