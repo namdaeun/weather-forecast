5bd25f6cc8c7db23feb6a7c120241cec0b57f311
namdaeun <nde40345@gmail.com>
2025-02-05 04:00:19 +0900

    feat: 시간 포맷 유틸함수 구현

---
+++ src/util/formatTime.ts
@@ -0,0 +1,9 @@
+export const formatTime = (dateTime: string): string => {
+ const [date, time] = dateTime.split(' ');
+ const [hour, minute] = time.split(':').map(Number);
+
+ const AMPM = hour >= 12 ? 'PM' : 'AM';
+ const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
+
+ return `${AMPM} ${formattedHour}:${minute.toString().padStart(2, '0')}`;
+};
