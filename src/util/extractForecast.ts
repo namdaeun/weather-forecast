c6dbe320d0259008bc0c3a39a8bd043d79d55b9e
namdaeun <nde40345@gmail.com>
2025-02-05 04:00:01 +0900

    fix: 특정 시간 데이터만 추출하도록 수정

--- src/util/extractForecast.ts
+++ src/util/extractForecast.ts
@@ -1,5 +1,9 @@
 import type { HourlyWeather } from '../type/forecast';
 
 export const extractSixHourlyForecast = (hourlyData: HourlyWeather[]) => {
- return hourlyData.filter((_, index) => index % 6 === 0);
+ return hourlyData.filter(item => {
+  const time = item.time.split(' ')[1];
+
+  return ['06:00', '09:00', '12:00', '15:00', '18:00'].includes(time);
+ });
 };
