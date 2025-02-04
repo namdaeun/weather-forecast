8e9d7ab06a9f54780227089a42f5221c3becd80b
namdaeun <nde40345@gmail.com>
2025-02-05 07:11:01 +0900

    feat: query값을 인자로 받음

--- src/api/fetchForecast.ts
+++ src/api/fetchForecast.ts
@@ -1,16 +1,14 @@
-export const fetchForecast = async () => {
+export const fetchForecast = async (location: string) => {
  try {
   const response = await fetch(
-   `${import.meta.env.VITE_BASE_URL}/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=Seoul&days=7`,
+   `${import.meta.env.VITE_BASE_URL}/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=${location}&days=7`,
   );
 
   if (!response.ok) {
    throw new Error(`${response.status}`);
   }
 
-  const data = await response.json();
-
-  return data;
+  return response.json();
  } catch (error) {
   console.error(error);
  }
