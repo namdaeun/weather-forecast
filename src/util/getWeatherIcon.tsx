c8040bd43736c653b93569669af46effb5eaf273
namdaeun <nde40345@gmail.com>
2025-02-05 08:53:28 +0900

    feat: 아이콘 스타일 지정을 통한 반응형 추가

--- src/util/getWeatherIcon.tsx
+++ src/util/getWeatherIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { css } from '@emotion/react';
 import {
  IcClouds,
  IcCloudsSnow,
@@ -8,32 +9,43 @@ import {
  IcSun,
  IcSunCloudsRain,
 } from '../asset/svg';
+import { MOBILE_MEDIA_QUERY } from '../style/mediaQuery';
 import type { Weather } from '../type/forecast';
+
+const iconStyle = (size: number) =>
+ css({
+  width: size,
+  height: size,
+  flexShrink: 0,
+
+  [`@media ${MOBILE_MEDIA_QUERY}`]: {
+   width: size * 0.7,
+   height: size * 0.7,
+  },
+ });
 
 export const getWeatherIcon = (weather: Weather, size: number) => {
  switch (weather) {
   case 'Partly Cloudy':
   case 'Overcast':
   case 'Cloudy':
-   return <IcClouds width={size} height={size} css={{ flexShrink: '0' }} />;
+   return <IcClouds width={size} height={size} css={iconStyle(size)} />;
   case 'Clear':
   case 'Sunny':
-   return <IcSun width={size} height={size} css={{ flexShrink: '0' }} />;
+   return <IcSun width={size} height={size} css={iconStyle(size)} />;
   case 'Light freezing rain':
   case 'Mist':
-   return (
-    <IcSunCloudsRain width={size} height={size} css={{ flexShrink: '0' }} />
-   );
+   return <IcSunCloudsRain width={size} height={size} css={iconStyle(size)} />;
   case 'Moderate snow':
   case 'Patchy snow nearby':
   case 'Light snow showers':
-   return <IcCloudsSnow width={size} height={size} css={{ flexShrink: '0' }} />;
+   return <IcCloudsSnow width={size} height={size} css={iconStyle(size)} />;
   case 'Blizzard':
   case 'Moderate or heavy snow showers':
-   return <IcSnow width={size} height={size} css={{ flexShrink: '0' }} />;
+   return <IcSnow width={size} height={size} css={iconStyle(size)} />;
   case 'Rain':
-   return <IcRain width={size} height={size} css={{ flexShrink: '0' }} />;
+   return <IcRain width={size} height={size} css={iconStyle(size)} />;
   default:
-   return <IcSun width={size} height={size} css={{ flexShrink: '0' }} />;
+   return <IcSun width={size} height={size} css={iconStyle(size)} />;
  }
 };
