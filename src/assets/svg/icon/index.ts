13e2855eda1d8a5b1cb69f3e0c7d2db2c5772466
namdaeun <nde40345@gmail.com>
2025-02-05 03:36:56 +0900

    docs: 아이콘 컴포넌트 위치 변경

--- src/assets/svg/icon/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as IcMap } from "./IcMap";
-export { default as IcSearch } from "./IcSearch";
-export { default as IcSun } from "./IcSun";
-export { default as IcWeather } from "./IcWeather";
