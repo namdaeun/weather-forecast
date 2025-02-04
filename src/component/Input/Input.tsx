5678f6ea70ce329f9af3e92445ebb7077669fe25
namdaeun <nde40345@gmail.com>
2025-02-05 07:38:22 +0900

    feat: input ref 전달하여 외부에서 접근 가능하게 함

--- src/component/Input/Input.tsx
+++ src/component/Input/Input.tsx
@@ -1,22 +1,29 @@
-import React, { type InputHTMLAttributes, type ReactNode } from 'react';
+import React, {
+ forwardRef,
+ type InputHTMLAttributes,
+ type ReactNode,
+} from 'react';
 import * as S from './Input.style';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
  leftIcon?: ReactNode;
 }
 
-const Input = ({ leftIcon, placeholder, onChange, ...props }: InputProps) => {
- return (
-  <div css={S.inputWrapperStyle}>
-   {leftIcon}
-   <input
-    css={S.inputStyle}
-    placeholder={placeholder}
-    onChange={onChange}
-    {...props}
-   />
-  </div>
- );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+ ({ leftIcon, placeholder, onChange, ...props }, ref) => {
+  return (
+   <div css={S.inputWrapperStyle}>
+    {leftIcon}
+    <input
+     ref={ref}
+     css={S.inputStyle}
+     placeholder={placeholder}
+     onChange={onChange}
+     {...props}
+    />
+   </div>
+  );
+ },
+);
 
 export default Input;
