76308623fa57677ec5b34f9a5bcf673f6bc215aa
namdaeun <nde40345@gmail.com>
2025-02-05 03:04:12 +0900

    feat: api 데이터 디폴트 값 상수 생성

---
+++ src/constants/default.ts
@@ -0,0 +1,39 @@
+import type { Current, Forecast, HourlyWeather } from '../type/forecast';
+
+export const DEFAULT_CURRENT: Current = {
+ temp_c: 0,
+ feelslike_c: 0,
+ wind_kph: 0,
+ uv: 0,
+ condition: { text: 'Clear' },
+};
+
+export const DEFAULT_FORECAST: Forecast = {
+ forecastday: [
+  {
+   date: '',
+   day: {
+    maxtemp_c: 0,
+    mintemp_c: 0,
+    daily_chance_of_rain: 0,
+    daily_chance_of_snow: 0,
+    temp_c: 0,
+    feelslike_c: 0,
+    wind_kph: 0,
+    uv: 0,
+    condition: { text: 'Clear' },
+   },
+   hour: [],
+  },
+ ],
+};
+
+export const DEFAULT_HOUR: HourlyWeather[] = [
+ {
+  time: '',
+  temp_c: 0,
+  condition: {
+   text: 'Clear',
+  },
+ },
+];
