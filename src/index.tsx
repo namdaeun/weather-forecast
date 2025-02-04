f46d45b05288c98191119dd06ebe355581a35b92
namdaeun <nde40345@gmail.com>
2025-02-04 23:31:21 +0900

    docs: App -> Index 파일명 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,21 @@
-import React, { useState } from 'react';
-import Map from './Map/Map';
+import React from 'react';
 import Weather from './Weather/Weather';
 import IcSearch from './assets/svg/icon/IcSearch';
 import Input from './components/Input/Input';
-import Navbar from './components/Navbar/Navbar';
 import * as S from './index.style';
-import type { NavItem } from './type/nav';
 
-function App() {
- const [selectedItem, setSelectedItem] = useState<NavItem>('weather');
-
+const Index = () => {
  return (
   <div css={S.wrapperStyle}>
-   <Navbar selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
-
    <div css={{ width: '100%' }}>
     <Input
      placeholder="search"
      leftIcon={<IcSearch width={15} height={15} />}
     />
-    {selectedItem === 'weather' && <Weather />}
-    {selectedItem === 'map' && <Map />}
+    <Weather />
    </div>
   </div>
  );
-}
+};
 
-export default App;
+export default Index;
