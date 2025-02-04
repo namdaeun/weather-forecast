f6033af35e91d6c1594f16ffd763f163aadaebdd
namdaeun <nde40345@gmail.com>
2025-02-05 08:50:10 +0900

    feat: 시간 별 일기 예보 반응형 추가

--- src/shared/HourlyForecast/HourlyForecast.style.ts
+++ src/shared/HourlyForecast/HourlyForecast.style.ts
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import { MOBILE_MEDIA_QUERY } from '../../style/mediaQuery';
 import { theme } from '../../style/theme';
 
 export const layoutStyle = css({
@@ -7,12 +8,24 @@ export const layoutStyle = css({
  padding: '5.7rem 1rem',
 
  gap: '2rem',
+
+ [`@media ${MOBILE_MEDIA_QUERY}`]: {
+  padding: '2rem 1rem',
+  flexDirection: 'column',
+  gap: '1rem',
+ },
 });
 
 export const hourStyle = css({
  color: theme.colors.gray300,
 
  ...theme.fonts.body2,
+
+ [`@media ${MOBILE_MEDIA_QUERY}`]: {
+  textAlign: 'center',
+
+  ...theme.fonts.body8,
+ },
 });
 
 export const forecastStyle = css({
@@ -24,10 +37,19 @@ export const forecastStyle = css({
  alignItems: 'center',
 
  gap: '3rem',
+
+ [`@media ${MOBILE_MEDIA_QUERY}`]: {
+  flexDirection: 'row',
+  gap: '1rem',
+ },
 });
 
 export const temperatureStyle = css({
  color: theme.colors.gray200,
 
  ...theme.fonts.body1,
+
+ [`@media ${MOBILE_MEDIA_QUERY}`]: {
+  ...theme.fonts.body8,
+ },
 });
