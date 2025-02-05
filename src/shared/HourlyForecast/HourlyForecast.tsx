f02f9ed7a91c8ef151bd015644ca0c5ddf8963bf
namdaeun <nde40345@gmail.com>
2025-02-06 00:34:31 +0900

    feat: Divider 적용

--- src/shared/HourlyForecast/HourlyForecast.tsx
+++ src/shared/HourlyForecast/HourlyForecast.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '../../component/Container/Container';
+import Divider from '../../component/Divider/Divider';
 import type { HourlyWeather, Weather } from '../../type/forecast';
 import { formatTime } from '../../util/formatTime';
 import { getWeatherImg } from '../../util/getWeatherIcon';
@@ -12,11 +13,16 @@ const HourlyForecast = ({
   <Container title="시간 별 일기예보">
    <div css={S.layoutStyle}>
     {hourlyForecast.map((hour, index) => (
-     <div key={`${hour.time}-${index}`} css={S.forecastStyle}>
-      <p css={S.hourStyle}>{formatTime(hour.time)}</p>
-      <p>{getWeatherImg(hour.condition.text.trim() as Weather, 100)}</p>
-      <p css={S.temperatureStyle}>{hour.temp_c}°C</p>
-     </div>
+     <>
+      <div key={`${hour.time}-${index}`} css={S.forecastStyle}>
+       <p css={S.hourStyle}>{formatTime(hour.time)}</p>
+       <p>{getWeatherImg(hour.condition.text.trim() as Weather, 100)}</p>
+       <p css={S.temperatureStyle}>{hour.temp_c}°C</p>
+      </div>
+      {index !== hourlyForecast.length - 1 && (
+       <Divider size={100} type="vertical" />
+      )}
+     </>
     ))}
    </div>
   </Container>
--- src/shared/WeeklyForecast/WeeklyForecast.tsx
+++ src/shared/WeeklyForecast/WeeklyForecast.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '../../component/Container/Container';
+import Divider from '../../component/Divider/Divider';
 import type { Forecast, Weather } from '../../type/forecast';
 import { formatDate } from '../../util/formatDate';
 import { getWeatherImg } from '../../util/getWeatherIcon';
@@ -10,12 +11,10 @@ const WeeklyForecast = ({ forecast }: { forecast?: Forecast }) => {
   return <div>loading</div>;
  }
 
- console.log(forecast.forecastday.map(item => item.day.condition.text));
-
  return (
   <Container title="7-Days Forecast" css={S.containerStyle}>
    <div css={S.wrapperStyle}>
-    {forecast.forecastday.map(day => (
+    {forecast.forecastday.map((day, index) => (
      <div key={day.date}>
       <div css={S.layoutStyle}>
        <p css={S.dateStyle}>{formatDate(day.date)}</p>
@@ -24,6 +23,9 @@ const WeeklyForecast = ({ forecast }: { forecast?: Forecast }) => {
         {day.day.maxtemp_c} / {day.day.mintemp_c}
        </p>
       </div>
+      {index !== forecast.forecastday.length - 1 && (
+       <Divider size={100} type="horizontal" />
+      )}
      </div>
     ))}
    </div>
